ba58b294680bbe7f0fd7cefbbfcd9f8b5112d16e
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2022-12-03 15:12:39 -0300

    inicio do post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,25 @@
+import styles from './Post.module.css';
+
 export function Post(props) {
     return (
-      <div>
-        <strong>{props.author}</strong>
-        <p>{props.content}</p>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+        <div  className={styles.avatar} src='https://avatars.githubusercontent.com/u/59962559?v=4'></div>
+          <div className={styles.authorInfo}>
+            <strong>Luiz Valentin</strong>
+            <span>Dev Master</span>
+          </div>
+        </div>
+        <time title='11 de novembro às 08:30h' dateTime='2002-11-11 08:30:30'>Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
       </div>
+    </article>
     )
   }
