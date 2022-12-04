5945dca7084ffde88b5c39a0c7de013db524cd5c
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2022-12-04 20:51:44 -0300

    finalizando o Post v1 sem o comentario

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,11 @@
 import styles from './Post.module.css';
 
 export function Post(props) {
-    return (
+  return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-        <div  className={styles.avatar} src='https://avatars.githubusercontent.com/u/59962559?v=4'></div>
+          <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/59962559?v=4' />
           <div className={styles.authorInfo}>
             <strong>Luiz Valentin</strong>
             <span>Dev Master</span>
@@ -14,12 +14,21 @@ export function Post(props) {
         <time title='11 de novembro às 08:30h' dateTime='2002-11-11 08:30:30'>Publicado há 1h</time>
       </header>
       <div className={styles.content}>
-      <p></p>
-      <p></p>
-      <p></p>
-      <p></p>
-      <p></p>
+      <p>batata é muito bom meu parceiro</p>
       </div>
+     
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
     </article>
-    )
-  }
+  )
+}
