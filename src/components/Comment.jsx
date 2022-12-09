823f78a09c158d651a51a2e838ab75362d1b1aa3
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2022-12-08 21:44:58 -0300

    like no comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,19 @@
 import styles from './Comment.module.css';
-
+import { useState } from "react";
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
 export function Comment({ content,onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
     onDeleteComment(content);
   }
-
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/59962559?v=4" alt="" />
@@ -30,9 +35,9 @@ export function Comment({ content,onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Util <span>{likeCount}</span>
           </button>
         </footer>
       </div>
