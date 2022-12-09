6b313e15d4612aa62721414707e8dc84f526e639
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2022-12-08 21:44:49 -0300

    bloqueio de comentario vaizio

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,7 +18,7 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
   });
 
-  function handleCrateNewComment() {
+  function handleCrateNewComment() { 
     event.preventDefault()
 
     setComments([...comments, newCommentText]);
@@ -26,7 +26,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório !');
   }
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
@@ -41,6 +46,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -81,9 +88,11 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
         </footer>
       </form>
       <div className={styles.commentList}>
@@ -99,4 +108,4 @@ export function Post({ author, publishedAt, content }) {
       </div>
     </article>
   )
-}
+}31
