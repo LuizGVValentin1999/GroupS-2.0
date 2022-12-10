2ab3c47e523e203a7c5589164431b2fad7f1ff97
Luiz Valentin <luiz.gustavo1999@hotmail.com>
2022-12-10 22:20:42 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,47 @@
-import './global.css'
-import styles from './App.module.css';
-import { Header } from './components/header'
-import { Post } from './components/Post'
-import { Sidebar } from './components/Sidebar'
-
+import "./global.css";
+import styles from "./App.module.css";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 const posts = [
   {
     id: 1,
     author: {
-      avatarUrl: 'https://avatars.githubusercontent.com/u/59962559?v=4',
-      name: 'Luiz Gustavo Valter Valentin',
-      role: 'Dev Master'
+      avatarUrl: "https://avatars.githubusercontent.com/u/59962559?v=4",
+      name: "Luiz Gustavo Valter Valentin",
+      role: "Dev Master",
     },
     content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um comit do meu projeto group S 2.0' },
-      { type: 'link', content: 'teste/link' },
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um comit do meu projeto group S 2.0",
+      },
+      { type: "link", content: "teste/link" },
     ],
-    publishedAt: new Date('2022-12-03 20:00:00'),
+    publishedAt: new Date("2022-12-03 20:00:00"),
   },
   {
     id: 2,
     author: {
-      avatarUrl: 'https://avatars.githubusercontent.com/u/59962559?v=4',
-      name: 'Luiz Gustavo Valter Valentin',
-      role: 'Dev Master'
+      avatarUrl: "https://avatars.githubusercontent.com/u/59962559?v=4",
+      name: "Luiz Gustavo Valter Valentin",
+      role: "Dev Master",
     },
     content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um comit do meu projeto group S 2.0' },
-      { type: 'link', content: 'teste/link' },
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um comit do meu projeto group S 2.0",
+      },
+      { type: "link", content: "teste/link" },
     ],
-    publishedAt: new Date('2022-12-06 20:00:00'),
+    publishedAt: new Date("2022-12-06 20:00:00"),
   },
 ];
 
 function App() {
-
   return (
     <div>
       <Header />
@@ -45,7 +49,7 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          {posts.map(post => {
+          {posts.map((post) => {
             return (
               <Post
                 key={post.id}
@@ -53,12 +57,12 @@ function App() {
                 content={post.content}
                 publishedAt={post.publishedAt}
               />
-            )
+            );
           })}
         </main>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
